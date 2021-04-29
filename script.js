131e9ed31b947b16378ecb993801a04a5f4d779e
Evan Kwan <evanyuekwan@gmail.com>
2021-04-29 17:52:53 -0400

    update pseudo code to include more stretch goals

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const url = new URL('http://ws.audioscrobbler.com/2.0/');
 
+// last.fm API key: 3f350f5eaa519a05c6b21c5afc810ec0
+// giphy API key: jGXNQaBmzgNmpV5vrHUt2lcq0QMg7F1s
+
+// test run of the API
 url.search = new URLSearchParams({
 	method: 'artist.getInfo',
 	artist: 'cher',
@@ -23,18 +27,22 @@ fetch(url)
 // get similar artist recommendations from API
 
 // stretch goals
-// autocomplete widget - get artist data from API while user types and allow user to select from drop down to search for similar artists
-// display titles of top 5 tracks of recommended artist
-// explore option - display 10-20 tags on the landing page. when the user clicks on a tag, display the top artists for that tag
+// autocomplete widget - when the user stops typing in the search input, display a drop down of suggested artist searches by making an API call with the value in the search input
+// display the top 5 tracks of recommended artist when the user interacts with the displayed artist
+// explore option - under the search bar there are genre tags listed. when the user clicks on a tag, display the top artists for that tag
+// get bio by making another API call using the artist.getInfo method
 // add artist images using the GIPHY API
 
-// pseudo code
+
+/////////////////
+// PSEUDO CODE //
+/////////////////
 
 // a landing page with the app heading " " and a welcome message " "
 
 // under the heading there is a form with a search bar and a submit button
 
-//// STRETCH GOAL-  under the search bar there are 10-20 tags listed. when the user clicks on a tag, display the top artists for that tag
+//// STRETCH GOAL-  under the search bar there are genre tags listed. when the user clicks on a tag, display the top artists for that tag
 
 //// STRETCH GOAL - when the user stops typing in the search input, display a drop down of suggested artist searches by making an API call with the value in the search input
 
@@ -44,13 +52,15 @@ fetch(url)
 
 // create <li> elements using forEach() for each artist and append to the page on the <ul>
 
-// add a click event of the individual <li>'s to make an API call toget similar artist recommendations based on which displayed artist was clicked
+// add a click event of the individual <li>'s to make an API call tog et similar artist recommendations based on which displayed artist was clicked
 
 // store the user selection in a variable
 
 // clear the <ul>
 
-// using forEach() create <li> elements for each similar artist and get bio by making another API call using the artist.getInfo method
+// using forEach() create <li> elements for each similar artist 
+
+//// STRETCH GOAL - get bio by making another API call using the artist.getInfo method
 
 // append the recommended artists to the page on the <ul>
 
