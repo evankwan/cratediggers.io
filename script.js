c47e59f9aa25782acd90521a68e7cc33e60b2ea1
Clement Sung <clement.sung7@gmail.com>
2021-04-29 21:52:30 -0400

    finished JS logic for artist recommendation API call

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const url = new URL('http://ws.audioscrobbler.com/2.0/');
 // last.fm API key: 3f350f5eaa519a05c6b21c5afc810ec0
 // giphy API key: jGXNQaBmzgNmpV5vrHUt2lcq0QMg7F1s
 
-app.getArtistsInfo = (query) => {
+app.getArtistsInfo = (query, searchMethod) => {
 	url.search = new URLSearchParams({
-		method: 'artist.search',
+		method: searchMethod,
 		artist: query,
 		api_key: '3f350f5eaa519a05c6b21c5afc810ec0',
 		limit: 10,
@@ -24,33 +24,46 @@ app.getArtistsInfo = (query) => {
 		.then((response) => {
 			return response.json();
 		}).then((data) => {
-			app.displayArtistsInfo(data);
+			app.displayArtistsInfo(data, searchMethod);
 		})
 }
 
-app.displayArtistsInfo = (data) => {
-	const artistsResults = data.results.artistmatches.artist;
+
+app.displayArtistsInfo = (data, searchMethod) => {
+	let artistsResults;
+	if(searchMethod === 'artist.search'){
+		artistsResults = data.results.artistmatches.artist
+	} else if (searchMethod === 'artist.getSimilar'){
+		artistsResults = data.similarartists.artist
+	}
 	resultsList.innerHTML = '';
+
 	artistsResults.forEach( (artist) => {
 		const artistContainer = document.createElement('li');
-		artistContainer.textContent = artist.name;
+		let artistInfo;
+
+		if(searchMethod === 'artist.search'){
+			artistInfo = artist.name;
+			artistContainer.addEventListener('click', function() {
+				app.getArtistsInfo(this.textContent, 'artist.getSimilar');
+			})
+		}else if(searchMethod === 'artist.getSimilar'){
+			artistInfo = `${artist.name} 
+			<a href="${artist.url}">check out their last.fm page</a> `
+		}
+
+		artistContainer.innerHTML = artistInfo;
 		resultsList.append(artistContainer);
-		artistContainer.addEventListener('click', () => {
-			app.getSimilarArtists();
-		})
 	})
 }
 
-app.getSimilarArtists = () => {
-	console.log('hello');
-}
 
 app.init = () => {
 	button.addEventListener('click', (event) => {
 		event.preventDefault();
 
 		const searchValue = searchInput.value;
-		app.getArtistsInfo(searchValue);
+		app.getArtistsInfo(searchValue,'artist.search');
 	})
 }
 
